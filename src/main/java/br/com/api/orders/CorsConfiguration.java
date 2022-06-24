957ea93edbbfd180ccb3f541c3224055c71e9ebe
Pam18 <yha.lima@outlook.com>
2022-06-24 12:24:42 -0300

    fix: ajuste no Cors

--- src/main/java/br/com/api/orders/CorsConfiguration.java
+++ src/main/java/br/com/api/orders/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("CORS_URL")
+                .allowedOrigins(System.getenv("CORS_URL"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
